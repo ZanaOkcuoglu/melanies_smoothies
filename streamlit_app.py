0413c0932d59e62a2730fe47be07141e6088b1ff
ZanaOkcuoglu <43749118+ZanaOkcuoglu@users.noreply.github.com>
2024-08-29 11:39:34 +0200

    Update streamlit_app.py
    v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,11 +39,7 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     # Combine selected ingredients into a single string
     ingredients_string = ', '.join(ingredients_list)
-    for fruit_chosen in ingredients_list:
-        ingredients_list += fruit_chosen + ''
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 
         
     # Prepare the SQL insert statement using safe string formatting
@@ -68,3 +64,13 @@ if ingredients_list:
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response)
 #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+if ingredients_list:
+    ingredients_string = ''
+
+    
+    for fruit_chosen in ingredients_list:
+        ingredients_list += fruit_chosen + ''
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
